ed3a5cb821070bab3ef0c80c3330a4999feb7940
Dot010 <jonathan2500@outlook.pt>
2026-09-22 18:32:28 -0300

    feat: add cart item count to Banner component

--- src/Components/Banner/index.tsx
+++ src/Components/Banner/index.tsx
@@ -2,28 +2,41 @@ import { Link } from 'react-router-dom'
 import { HeroContainer, HeaderBar, Logo, Titulo } from './style'
 import LogoImage from '../../assets/images/logo.png'
 
+
+import { useSelector } from 'react-redux'
+import type { RootState } from '../../store'
+
+
 type Props = {
   variant?: 'home' | 'perfil'
 }
 
-const Banner = ({ variant = 'home' }: Props) => (
-  <HeroContainer $variant={variant}>
-    {variant === 'home' ? (
-      <>
-        <Logo src={LogoImage} alt="efood" />
-        <Titulo>
-          Viva experiências gastronômicas <br />
-          no conforto da sua casa
-        </Titulo>
-      </>
-    ) : (
-      <HeaderBar className="container">
-        <Link to="/">Restaurantes</Link>
-        <Logo src={LogoImage} alt="efood" />
-        <span>0 produto(s) no carrinho</span>
-      </HeaderBar>
-    )}
-  </HeroContainer>
-)
+const Banner = ({ variant = 'home' }: Props) => 
+  {
+  
+const totalItems = useSelector((state: RootState) => state.cart.items.length);
+
+    
+  return (
+    <HeroContainer $variant={variant}>
+      {variant === 'home' ? (
+        <>
+          <Logo src={LogoImage} alt="efood" />
+          <Titulo>
+            Viva experiências gastronômicas <br />
+            no conforto da sua casa
+          </Titulo>
+        </>
+      ) : (
+        <HeaderBar className="container">
+          <Link to="/">Restaurantes</Link>
+          <Logo src={LogoImage} alt="efood" />
+          <span>{totalItems} produto(s) no carrinho</span>
+        </HeaderBar>
+      )}
+    </HeroContainer>
+  )
+}
+
 
 export default Banner
